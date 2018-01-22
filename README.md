d4ac218bbf783683d338e6dd123f84fff340c466
Admin <mc@csgo.cash>
2018-01-22 17:11:28 +0200

    Fixed DI configuration

--- README.md
+++ README.md
@@ -25,6 +25,42 @@ JWT_USER_IDENTITY_FIELD='email'
 JWT_USER_CLASS='Garlic\Auth\Security\User'
 ```
 
+#### 3. Add Security configuration (change packages/security.yml):
+```yaml
+security:
+    providers:
+        jwt_provider:
+            lexik_jwt:
+                class: Garlic\Auth\Security\User
+    firewalls:
+        main:
+            pattern:   ^/
+            stateless: true
+            anonymous: true
+            guard:
+                authenticators:
+                    - lexik_jwt_authentication.jwt_token_authenticator
+```
+
+#### 4. Add Lexik jwt bundle configuration (change lexik_jwt_authentication.yml):
+```yaml
+lexik_jwt_authentication:
+    private_key_path: '%env(JWT_PRIVATE_KEY_PATH)%'
+    public_key_path:  '%env(JWT_PUBLIC_KEY_PATH)%'
+    pass_phrase:      '%env(JWT_PASSPHRASE)%'
+    token_ttl:        '%env(JWT_TOKEN_TTL)%'
+    user_identity_field: email
+    encoder:
+        service: lexik_jwt_authentication.encoder.lcobucci
+        crypto_engine:  openssl
+        signature_algorithm: RS384
+    token_extractors:
+        authorization_header:
+            enabled: true
+            prefix:  ''
+            name:    Authorization
+```
+
 ### Using
 
 #### 1. Make JWT token from the authorization service
--- src/DependencyInjection/GarlicAuthExtension.php
+++ src/DependencyInjection/GarlicAuthExtension.php
@@ -11,7 +11,7 @@ use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 /**
  * Class AuthorizationExtension
  */
-class GarlicAuthExtension extends Extension implements PrependExtensionInterface
+class GarlicAuthExtension extends Extension
 {
     /**
      * {@inheritdoc}
@@ -23,59 +23,5 @@ class GarlicAuthExtension extends Extension implements PrependExtensionInterface
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function prepend(ContainerBuilder $container)
-    {
-        # Config for JWT bundle
-        $jwtConfig = [
-            'private_key_path' => getenv('JWT_PRIVATE_KEY_PATH'),
-            'public_key_path' => (getenv('JWT_PUBLIC_KEY_PATH')) ? getenv('JWT_PUBLIC_KEY_PATH') : '%kernel.root_dir%/../config/jwt/public.pem',
-            'pass_phrase' => getenv('JWT_KEY_PASS_PHRASE'),
-            'token_ttl' => (getenv('JWT_TOKEN_TTL')) ? getenv('JWT_TOKEN_TTL') : 3600,
-            'user_identity_field' => getenv('JWT_USER_IDENTITY_FIELD') ?? 'email',
-            'encoder' => [
-                'service' => 'lexik_jwt_authentication.encoder.lcobucci',
-                'crypto_engine' => 'openssl',
-                'signature_algorithm' => 'RS384',
-            ],
-            'token_extractors' => [
-                'authorization_header' => [
-                    'enabled' => true,
-                    'prefix' => '',
-                    'name' => 'Authorization',
-                ]
-            ]
-        ];
-
-        $container->prependExtensionConfig('lexik_jwt_authentication', $jwtConfig);
-
-        # Security settings for Auth bundle
-        $securityConfig = [
-            'providers' => [
-                'authorization_header' => [
-                    'lexik_jwt' => [
-                        'class' => (getenv('JWT_USER_CLASS')) ?? 'Garlic\Auth\Security\User'
-                    ],
-                ]
-            ],
-            'firewalls' => [
-                'main' => [
-                    'pattern' => '^/',
-                    'stateless' => true,
-                    'anonymous' => true,
-                    'guard' => [
-                        'authenticators' => [
-                            'lexik_jwt_authentication.jwt_token_authenticator'
-                        ],
-                    ]
-                ]
-            ]
-        ];
-
-        $container->prependExtensionConfig('security', $securityConfig);
     }
 }
